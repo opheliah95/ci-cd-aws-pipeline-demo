4b56df5408cb133d038585943f367ccde054618c
opheliah95 <opheliah95@gmail.com>
2022-06-06 16:47:13 +0100

    feat: added env vars

--- bin/ci-cd-aws-pipeline-demo.ts
+++ bin/ci-cd-aws-pipeline-demo.ts
@@ -1,14 +1,16 @@
-#!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { CiCdAwsPipelineDemoStack } from '../lib/ci-cd-aws-pipeline-demo-stack';
+require('dotenv').config();
 
 const app = new cdk.App();
 new CiCdAwsPipelineDemoStack(app, 'CiCdAwsPipelineDemoStack', {
   env: {
-    account: '755314965794',
-    region: 'us-east-1',
+    account: process.env.ACCOUNT_ID,
+    region: process.env.REGION,
   }
 });
 
 app.synth();
+
+console.log(process.env.ACCOUNT_ID)
--- lib/ci-cd-aws-pipeline-demo-stack.ts
+++ lib/ci-cd-aws-pipeline-demo-stack.ts
@@ -3,15 +3,16 @@ import { Construct } from 'constructs';
 import { CodePipeline, CodePipelineSource, ShellStep, Step } from 'aws-cdk-lib/pipelines';
 import { ManualApprovalStep } from 'aws-cdk-lib/pipelines';
 import { MyPipelineAppStage } from './stage';
+require('dotenv').config();
 
 export class CiCdAwsPipelineDemoStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-
+    let repo:string = process.env.REP || ''; 
     const pipeline = new CodePipeline(this, 'Pipeline', {
       pipelineName: 'TestPipeline',
       synth: new ShellStep('Synth', {
-        input: CodePipelineSource.gitHub('beabetterdevv/ci-cd-aws-pipeline-demo', 'main'), //Remember to change 
+        input: CodePipelineSource.gitHub(repo, 'main'), //Remember to change 
         commands: ['npm ci', 
                    'npm run build', 
                    'npx cdk synth']
